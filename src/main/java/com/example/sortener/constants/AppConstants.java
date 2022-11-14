c22ee87079610e86a2f86656a8ff69028cac088f
Juraj Pacholsky <pacholskyj@gmail.com>
2022-11-14 09:34:47 +0100

    final version

--- src/main/java/com/example/sortener/constants/AppConstants.java
+++ src/main/java/com/example/sortener/constants/AppConstants.java
@@ -14,15 +14,15 @@ public final class AppConstants {
 
     public static final String HELP_PAGE = "HOW TO INSTALL \n \n\n 1. Run mvc clean package to create executable JAR file \n\n " +
             "2. JAR File is contained in root/target/shortener-0.0.1-SNAPSHOT.jar \n\n " +
-            "3. Find the file through any kind of cmd/terminal and run command java-jar-urlShortener-0.0.1-UrlShortener.jar \n\n " +
+            "3. Find the file through any kind of cmd/terminal and run command java -jar urlShortener-0.0.1-UrlShortener.jar \n\n " +
             "4. This will start up whole application \n\n " +
             "5. Add row (127.0.0.1) you your host file\n\n\n" +
             "HOW TO USE\n\n\n " +
             "1. Swagger documentation can be found at localhost:8080/swagger-ui/index.html -> Here you can find all endpoints. (/v2/api-docs)\n\n " +
             "2. For using app you need an account (/account) \n\n " +
             "3. Once account is create, endpoints are using basic auth (name/password)\n\n " +
-            "4. For shortening of URL please use /register endpoint whitch will return short version of URL \n\n " +
+            "4. For shortening of URL please use /register endpoint which will return short version of URL \n\n " +
             "5. Short URL has 2 parts: (http://short.com - localhost:8080/) and Path variable shortUrl (10 alpha numeric characters finally: http://short.com/asdk238shr \n\n " +
-            "6. If shortUrl is called, statistics are incremented and there are avaible at /statistics ";
+            "6. If shortUrl is called, statistics are incremented and there are available at /statistics ";
 
 }
--- src/main/java/com/example/sortener/controller/HelpController.java
+++ src/main/java/com/example/sortener/controller/HelpController.java
@@ -1,22 +1,20 @@
 package com.example.sortener.controller;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static com.example.sortener.constants.AppConstants.HELP_PAGE;
-import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping
-@EnableWebMvc
 public class HelpController {
 
-    @GetMapping(path = "help")
-    public ResponseEntity<String> getHelp() {
-        return ok(HELP_PAGE);
+    @GetMapping(value = "help", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody String getHelp() {
+        return HELP_PAGE;
     }
 
 }
